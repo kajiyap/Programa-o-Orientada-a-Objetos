b12ef4906916e7e95c32d7c67a64670d9256cc35
Pedro Santos Kajiya <kajiyapedro@gmail.com>
2024-05-27 22:05:14 -0300

    adicionando o cadastro de telefone, rg e cpf e a atualização de dados do cliente

--- src/componentes/formularioCadastroCliente.js
+++ src/componentes/formularioCadastroCliente.js
@@ -2,21 +2,96 @@ export default function FormularioCadastroCliente(props) {
     let tema = props.tema
     return (
         <div className="container-fluid">
-            <form>
-                <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" />
+            <button style={{margin: "2vh"}} class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCliente" aria-expanded="false" aria-controls="collapseCliente">
+                Cadastro de cliente
+            </button>
+            <button style={{margin: "2vh"}} class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTel" aria-expanded="false" aria-controls="collapseTel">
+                Cadastro de telefone
+            </button>
+            <button style={{margin: "2vh"}} class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseRg" aria-expanded="false" aria-controls="collapseRg">
+                Cadastro de RG
+            </button>
+            <div class="row">
+                <div class="col">
+                    <div class="collapse" id="collapseCliente">
+                        <form style={{background: tema, padding: "3vh", borderRadius: "3vh", margin:"1vh", alignSelf: "flex-start"}}>
+                            <h5>Cadastro de cliente</h5>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Nome social" aria-label="Nome social" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="CPF" aria-label="CPF" aria-describedby="basic-addon1" />
+                                <input type="text" className="form-control" placeholder="Data de emissão do CPF" aria-label="Data de emissão do CPF" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text" id="basic-addon1" style={{ background: tema }}>@</span>
+                                <input type="text" className="form-control" placeholder="E-mail" aria-label="E-mail" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Nome social" aria-label="Nome social" aria-describedby="basic-addon1" />
+                <div class="col">
+                    <div class="collapse" id="collapseTel">
+                        <form style={{background: tema, padding: "3vh", borderRadius: "3vh",margin:"1vh", alignSelf: "flex-end"}}>
+                            <h5>Cadastro de telefone</h5>
+                            <div class="dropdown" style={{margin:"1vh"}}>
+                                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Selecione o cliente                        
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Cliente 1</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 2</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 3</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 4</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 5</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 6</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 7</a></li>
+                                </ul>
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="DDD" aria-label="DDD" aria-describedby="basic-addon1" />
+                                <input type="text" className="form-control" placeholder="Número de telefone" aria-label="Número de telefone" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="collapse" id="collapseRg">
+                        <form style={{background: tema, padding: "3vh", borderRadius: "3vh", margin:"1vh", alignSelf: "flex-end"}}>
+                            <h5>Cadastro de RG</h5>
+                            <div class="dropdown" style={{margin:"1vh"}}>
+                                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Selecione o cliente
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Cliente 1</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 2</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 3</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 4</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 5</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 6</a></li>
+                                    <li><a class="dropdown-item" href="#">Cliente 7</a></li>
+                                </ul>
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Numero do RG" aria-label="Numero do RG" aria-describedby="basic-addon1" />
+                                <input type="text" className="form-control" placeholder="Data de emissão do RG" aria-label="Data de emissão do RG" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div className="input-group mb-3">
-                    <span className="input-group-text" id="basic-addon1" style={{ background: tema }}>@</span>
-                    <input type="text" className="form-control" placeholder="E-mail" aria-label="E-mail" aria-describedby="basic-addon1" />
-                </div>
-                <div className="input-group mb-3">
-                    <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
-                </div>
-            </form>
+            </div>
         </div>
     )
 }
--- src/componentes/listaCliente.js
+++ src/componentes/listaCliente.js
@@ -4,12 +4,67 @@ export default function ListaCliente(props) {
     return (
         <div className="container-fluid">
             <div className="list-group">
-                <a href="#" className="list-group-item list-group-item-action">Cliente 1</a>
-                <a href="#" className="list-group-item list-group-item-action">Cliente 2</a>
-                <a href="#" className="list-group-item list-group-item-action">Cliente 3</a>
-                <a href="#" className="list-group-item list-group-item-action" style={{ backgroundColor: tema }} >Cliente 4</a>
-                <a href="#" className="list-group-item list-group-item-action">Cliente 5</a>
-                <a href="#" className="list-group-item list-group-item-action">Cliente 6</a>
+                <div style={{display: "flex", flexDirection: "row", margin: "0.5vh"}}>
+                    <a href="#" className="list-group-item list-group-item-action">Cliente 1</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
+                </div>
+                <div style={{display: "flex", flexDirection: "row", margin: "0.5vh"}}>
+                    <a href="#" className="list-group-item list-group-item-action">Cliente 2</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
+                </div>
+                <div style={{display: "flex", flexDirection: "row", margin: "0.5vh"}}>
+                    <a href="#" className="list-group-item list-group-item-action">Cliente 3</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
+                </div>
+                <div style={{display: "flex", flexDirection: "row", margin: "0.5vh"}}>
+                    <a href="#" className="list-group-item list-group-item-action">Cliente 4</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
+                </div>
+                <div style={{display: "flex", flexDirection: "row", margin: "0.5vh"}}>
+                    <a href="#" className="list-group-item list-group-item-action">Cliente 5</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
+                </div>
+                <div style={{display: "flex", flexDirection: "row", margin: "0.5vh"}}>
+                    <a href="#" className="list-group-item list-group-item-action">Cliente 6</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
+                </div>
+                <div style={{display: "flex", flexDirection: "row", margin: "0.5vh"}}>
+                    <a href="#" className="list-group-item list-group-item-action">Cliente 7</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
+                </div>
+            </div>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <h5>Atualização de cliente</h5>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Novo Nome" aria-label="Novo Nome" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Novo Nome social" aria-label="Novo Nome social" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Novo CPF" aria-label="Novo CPF" aria-describedby="basic-addon1" />
+                                <input type="text" className="form-control" placeholder="Novo Data de emissão do CPF" aria-label="Novo Data de emissão do CPF" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text" id="basic-addon1" style={{ background: tema }}>@</span>
+                                <input type="text" className="form-control" placeholder="Novo E-mail" aria-label="Novo E-mail" aria-describedby="basic-addon1" />
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-primary">Salvar alterações</button>
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
