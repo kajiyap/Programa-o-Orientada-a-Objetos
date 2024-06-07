da11887875e4adcc53ede84dcb769be6ecc6e787
Pedro Santos Kajiya <kajiyapedro@gmail.com>
2024-06-06 21:54:33 -0300

    Crud clientes

--- src/componentes/formularioCadastroCliente.tsx
+++ src/componentes/formularioCadastroCliente.tsx
@@ -10,22 +10,93 @@ export default class FormularioCadastroCliente extends Component<props> {
         let tema = this.props.tema
         return (
             <div className="container-fluid">
-                <form>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" />
+            <button style={{margin: "2vh"}} className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCliente" aria-expanded="false" aria-controls="collapseCliente">
+                Cadastro de cliente
+            </button>
+            <button style={{margin: "2vh"}} className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTel" aria-expanded="false" aria-controls="collapseTel">
+                Cadastro de telefone
+            </button>
+            <button style={{margin: "2vh"}} className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseRg" aria-expanded="false" aria-controls="collapseRg">
+                Cadastro de RG
+            </button>
+            <div className="row">
+                <div className="col">
+                    <div className="collapse" id="collapseCliente">
+                        <form style={{background: tema, padding: "3vh", borderRadius: "3vh", margin:"1vh", alignSelf: "flex-start"}}>
+                            <h5>Cadastro de cliente</h5>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Nome social" aria-label="Nome social" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="CPF" aria-label="CPF" aria-describedby="basic-addon1" />
+                                <input type="text" className="form-control" placeholder="Data de emissão do CPF" aria-label="Data de emissão do CPF" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text" id="basic-addon1" style={{ background: tema }}>@</span>
+                                <input type="text" className="form-control" placeholder="E-mail" aria-label="E-mail" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
+                            </div>
+                        </form>
                     </div>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Nome social" aria-label="Nome social" aria-describedby="basic-addon1" />
+                </div>
+                <div className="col">
+                    <div className="collapse" id="collapseTel">
+                        <form style={{background: tema, padding: "3vh", borderRadius: "3vh",margin:"1vh", alignSelf: "flex-end"}}>
+                            <h5>Cadastro de telefone</h5>
+                            <div className="dropdown" style={{margin:"1vh"}}>
+                                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Selecione o cliente                        
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Pedro</a></li>
+                                    <li><a className="dropdown-item" href="#">Matheus</a></li>
+                                    <li><a className="dropdown-item" href="#">Bruno</a></li>
+                                    <li><a className="dropdown-item" href="#">Cauã</a></li>
+                                    <li><a className="dropdown-item" href="#">Erick</a></li>
+                                </ul>
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="DDD" aria-label="DDD" aria-describedby="basic-addon1" />
+                                <input type="text" className="form-control" placeholder="Número de telefone" aria-label="Número de telefone" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
+                            </div>
+                        </form>
                     </div>
-                    <div className="input-group mb-3">
-                        <span className="input-group-text" id="basic-addon1" style={{ background: tema }}>@</span>
-                        <input type="text" className="form-control" placeholder="E-mail" aria-label="E-mail" aria-describedby="basic-addon1" />
+
+                    <div className="collapse" id="collapseRg">
+                        <form style={{background: tema, padding: "3vh", borderRadius: "3vh", margin:"1vh", alignSelf: "flex-end"}}>
+                            <h5>Cadastro de RG</h5>
+                            <div className="dropdown">
+                                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Selecione o cliente
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Pedro</a></li>
+                                    <li><a className="dropdown-item" href="#">Matheus</a></li>
+                                    <li><a className="dropdown-item" href="#">Bruno</a></li>
+                                    <li><a className="dropdown-item" href="#">Cauã</a></li>
+                                    <li><a className="dropdown-item" href="#">Erick</a></li>
+                                </ul>
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Numero do RG" aria-label="Numero do RG" aria-describedby="basic-addon1" />
+                                <input type="text" className="form-control" placeholder="Data de emissão do RG" aria-label="Data de emissão do RG" aria-describedby="basic-addon1" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
+                            </div>
+                        </form>
                     </div>
-                    <div className="input-group mb-3">
-                        <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
-                    </div>
-                </form>
+                </div>
             </div>
+        </div>
         )
     }
 }
--- src/componentes/listaClientes.tsx
+++ src/componentes/listaClientes.tsx
@@ -1,24 +1,29 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Component } from "react";
+import React, { Component } from 'react';
+import ElementoListaCliente from './elementoListaCliente';
 
-type props = {
-    tema: string
+interface ListaClienteProps {
+    tema: string;
 }
 
-export default class ListaCliente extends Component<props>{
+class ListaCliente extends Component<ListaClienteProps> {
     render() {
-        let tema = this.props.tema
+        const { tema } = this.props;
+        const rg = ["12.456.789-1", "32.654.987-1", "75.159.852-8"];
+        const telefones = ["(12)99257-7679", "(35)98874-6569", "(11)99874-8852"];
+
         return (
             <div className="container-fluid">
-                <div className="list-group">
-                    <a href="#" className="list-group-item list-group-item-action">Cliente 1</a>
-                    <a href="#" className="list-group-item list-group-item-action">Cliente 2</a>
-                    <a href="#" className="list-group-item list-group-item-action">Cliente 3</a>
-                    <a href="#" className="list-group-item list-group-item-action" style={{ backgroundColor: tema }} >Cliente 4</a>
-                    <a href="#" className="list-group-item list-group-item-action">Cliente 5</a>
-                    <a href="#" className="list-group-item list-group-item-action">Cliente 6</a>
-                </div>
+                <h1>Clientes</h1>
+                <p>Selecione um cliente para ver seus dados.</p>
+                <ElementoListaCliente nome="Pedro Kajiya" nomeSocial="Pedrovisk" CPF="123.456.789-10" email="pedrokajiya@email.com" tema={tema} telefone={telefones} rg={rg} />
+                <ElementoListaCliente nome="Matheus Madeira" nomeSocial="Madeira" CPF="159.753.852.78" email="matheusmadeira@email.com" tema={tema} telefone={telefones} rg={rg} />
+                <ElementoListaCliente nome="Bruno Silvério" nomeSocial="Bruno T.I" CPF="984.789.312-55" email="brunosilverio@email.com" tema={tema} telefone={telefones} rg={rg} />
+                <ElementoListaCliente nome="Caua Dezidera" nomeSocial="Brinquedo" CPF="158.456.565-88" email="cauad@email.com" tema={tema} telefone={telefones} rg={rg} />
+                <ElementoListaCliente nome="Erick Awata" nomeSocial="DJ Japão" CPF="122.455.788-10" email="djjapao@email.com" tema={tema} telefone={telefones} rg={rg} />
             </div>
-        )
+        );
     }
 }
+
+export default ListaCliente;
